2358f62f5380fd49e47fe8463f931f46add091a5
nfcarbone <nfcarbone@gmail.com>
2020-12-16 22:26:50 -0500

    cleanup client.js, test api

--- auth.js
+++ auth.js
@@ -5,61 +5,48 @@ const ObjectID = require('mongodb').ObjectID;
 const Client = require('./models/Client.model');
 const Provider = require('./models/Provider.model');
 
-module.exports = () => {
-  const localStrategy = (err, user, password, done) => {
-    console.log(email + ' attempted to log in.');
-    if (err) {
-      return done(err);
-    }
-    if (!user) {
-      return done(null, false);
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return done(null, false);
-    }
-    return done(null, user);
-  };
+// local strategy for both client and provider
+const localStrategy = (err, user, email, password, done) => {
+  console.log(email + ' attempted to log in.');
+  if (err) {
+    return done(err);
+  }
+  if (!user) {
+    return done(null, false);
+  }
+  if (!bcrypt.compareSync(password, user.password)) {
+    return done(null, false);
+  }
+  return done(null, user);
+};
 
+module.exports = () => {
+  // client local strategy for passport
   passport.use(
     'clientLocal',
-    new LocalStrategy(
-      {
-        usernameField: 'email',
-      },
-      (email, password, done) => {
-        Client.findOne({ email }, (err, user) => {
-          console.log(email + ' attempted to log in.');
-          if (err) {
-            return done(err);
-          }
-          if (!user) {
-            return done(null, false);
-          }
-          if (!bcrypt.compareSync(password, user.password)) {
-            return done(null, false);
-          }
-          return done(null, user);
-        });
-      }
-    )
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      Client.findOne({ email }, (err, user) => {
+        localStrategy(err, user, email, password, done);
+      });
+    })
   );
 
+  // provider local strategy for passport
   passport.use(
     'providerLocal',
-    new LocalStrategy(
-      {
-        usernameField: 'email',
-      },
-      (email, password, done) => {
-        Provider.findOne({ email }, localStrategy(err, user, password, done));
-      }
-    )
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      Provider.findOne({ email }, (err, user) => {
+        localStrategy(err, user, email, password, done);
+      });
+    })
   );
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
 
+  // if from client endpoint, check client collection
+  // if from provider endpoint, check provider collection
   passport.deserializeUser((req, id, done) => {
     let db = req.url.replace(/^\/(\w*).*/, '$1');
     if (db === 'client') {
@@ -67,7 +54,7 @@ module.exports = () => {
         if (err) console.log(err);
         done(null, doc);
       });
-    } else {
+    } else if (db === 'provider') {
       Provider.findOne({ _id: new ObjectID(id) }, (err, doc) => {
         if (err) console.log(err);
         done(null, doc);
--- routes/client.js
+++ routes/client.js
@@ -15,19 +15,26 @@ router.route('/locations').get((req, res) => {
     .catch((err) => res.status(400).json(err));
 });
 
-// add client to db with email and password
+// register new client
 router.route('/register').post((req, res) => {
+  // parse request
   const { email, password } = req.body;
+  // hash client password
   const hash = bcrypt.hashSync(password, 12);
+  // create new client from Client Schema
   const newClient = new Client({ email, password: hash });
+  // check if client already registered
   Client.findOne({ email: newClient.email })
     .then((client) => {
+      // if email exists, return false
       if (client) {
         return res.send(false);
       } else {
+        // save new client to db
         newClient
           .save()
           .then((user) => {
+            // login client to session
             req.login(user, (err) => {
               if (!err) {
                 return res.send('success');
@@ -42,7 +49,7 @@ router.route('/register').post((req, res) => {
     .catch((err) => res.status(400).send('find error'));
 });
 
-// login
+// client login
 router
   .route('/login')
   .post(passport.authenticate('clientLocal'), (req, res, next) => {
@@ -51,16 +58,21 @@ router
 
 // add an appointment
 router.route('/appointments').post(ensureAuthenticated, async (req, res) => {
+  // parse request
   const { client, name, dob, phone, date, time, location, test } = req.body;
+  // find requested location
   const dbLocation = await Location.findById(location, {}, (err) => {
     if (err) return res.send('location not found');
   });
   for (let appt of dbLocation.appointments) {
+    // if appointment date and time already taken
     if (appt.date === date && appt.time === time) {
       return res.send('unavailable');
     }
   }
+  // create new appointment with unique mongo _id
   const _id = new ObjectId();
+  // add it to location's appointments array
   dbLocation.appointments.unshift({
     date,
     time,
@@ -71,9 +83,11 @@ router.route('/appointments').post(ensureAuthenticated, async (req, res) => {
     phone,
     _id,
   });
+  // save location
   dbLocation
     .save()
     .then(async () => {
+      // adjust appointment for client's document
       const newAppointment = {
         date,
         time,
@@ -84,9 +98,11 @@ router.route('/appointments').post(ensureAuthenticated, async (req, res) => {
         test,
         confirmation: _id,
       };
+      // find client
       let dbClient = await Client.findById(client, {}, (err) => {
         if (err) return res.status(400).send('client not found');
       });
+      // add appointment to client's appointments array
       dbClient.appointments.unshift(newAppointment);
       dbClient
         .save()
@@ -96,39 +112,55 @@ router.route('/appointments').post(ensureAuthenticated, async (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
+// get all client appointments
 router.route('/appointments').get(ensureAuthenticated, async (req, res) => {
+  // parse request
   const client = req.body.client;
+  // find client
   const clientDoc = await Client.findById(client, {}, (err) => {
     if (err) return res.status(400).send('client not found');
   });
+  // return client appointments array
   return res.json(clientDoc.appointments);
 });
 
+// delete client appointment
 router.route('/appointments').delete(ensureAuthenticated, async (req, res) => {
+  // parse request
   const { client, location, confirmation } = req.body;
+  // require request fields
   if (!client || !location || !confirmation)
     return res.status(400).send('required fields missing');
+  // find appointment location
   const dbLocation = await Location.findById(location, {}, (err) => {
     if (err) return res.status(400).send('location not found');
   });
+  // confirmed false until we find the appointment
   let confirmed = false;
   for (let [index, appt] of Object.entries(dbLocation.appointments)) {
     if (appt._id.toString() === confirmation) {
+      // remove it from location's appointments array
       dbLocation.appointments.splice(index, 1);
       confirmed = true;
     }
   }
+  // if we don't find it, return bad request
   if (!confirmed) return res.status(400).send('invalid confirmation number');
+  // save location
   dbLocation
     .save()
     .then(async () => {
+      // find client
       let dbClient = await Client.findById(client, {}, (err) => {
         if (err) return res.status(400).send('client not found');
       });
+      // now look for appointment in client's appointments array
       for (let [index, appt] of Object.entries(dbClient.appointments)) {
         if (appt.confirmation.toString() === confirmation)
+          // remove it
           dbClient.appointments.splice(index, 1);
       }
+      // save client
       dbClient
         .save()
         .then((client) => res.json(client))
@@ -137,18 +169,25 @@ router.route('/appointments').delete(ensureAuthenticated, async (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
+// update client profile
 router.route('/update/:type').post(ensureAuthenticated, async (req, res) => {
+  // type of update
   const type = req.params.type;
+  // parse request, include only values to be updated
   let request = createTruthyObject(req.body);
   if (!request.client) return res.send('must provide client id');
+  // no need to add client field to db
   delete request.client;
+  // find client
   let dbClient = await Client.findById(req.body.client, (err) => {
     if (err) return res.status(400).send('location not found');
   });
+  // check type of update
   switch (type) {
     case 'insurance':
     case 'basic':
       for (let [key, val] of Object.entries(request)) {
+        // if value exists in request, change it in client document
         if (val) dbClient[key] = val;
       }
       break;
@@ -161,33 +200,37 @@ router.route('/update/:type').post(ensureAuthenticated, async (req, res) => {
       dbClient.password = request.password;
       break;
     case 'travel':
+      // add new travel history at front
       dbClient.travel = [request, ...dbClient.travel];
       break;
     default:
       return res.status(400).send('invalid update');
   }
+  // save client
   dbClient
     .save()
     .then((client) => res.json(client))
     .catch((err) => res.status(400).json(err));
 });
 
+// get client document
 router.route('/').get(ensureAuthenticated, (req, res) => {
+  // parse request
   let client = req.body.client;
-  console.log(client);
+  // find client
   Client.findById(client, {}, (err) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).send('client not found');
-    }
+    if (err) return res.status(400).send('client not found');
   }).then((client) => res.json(client));
 });
 
+// client logout *** where does this go???
 router.route('/logout').get((req, res) => {
+  console.log('trying');
   req.logout();
-  res.redirect('/');
+  res.redirect('/'); // ???
 });
 
+// catch all invalid endpoints
 router.use((req, res, next) => {
   res.status(404).type('text').send('Not Found');
 });
