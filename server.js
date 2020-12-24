0de9ca52285c1bc0381e3adbb23274977d7deca8
Nick Carbone <nfcarbone@gmail.com>
2020-12-24 00:31:35 -0500

    get location distances from google

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use((e, req, res, next) => {
 });
 
 // start
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   logger.info(`Server is running on port: ${port}`);
 });
