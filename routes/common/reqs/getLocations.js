ee0649b9cbe3727ae98e076e94d25018d893c448
nfcarbone <nfcarbone@gmail.com>
2021-01-02 13:36:44 -0500

    need to separate functions for getLocations/distance

--- routes/common/reqs/getLocations.js
+++ routes/common/reqs/getLocations.js
@@ -13,21 +13,25 @@ const getLocations = async (req, res) => {
     const updatedLocations = await getDistance(req.query.zip, locationZips, [
       ...locations,
     ]);
-    return res.status(200).json(updatedLocations);
+    if (updatedLocations[0]) return res.status(400).send(updatedLocations[0]);
+    return res.status(200).json(updatedLocations[1]);
   } catch (e) {
     logger.error(`/locations => \n ${e.stack}`);
-    return res.status(500).send('An error occurred. Please try again later.');
+    return res.status(500).send(e.message);
   }
 };
 
 const getDistance = async (zip, locationZips, locations) => {
   const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${zip}&destinations=${locationZips}&key=${process.env.MAPS_API}`;
   const distanceData = await axios.get(url);
+  if (distanceData.data.origin_addresses[0] === '') {
+    return ['Invalid zip code.', null];
+  }
   const distance = distanceData.data.rows[0].elements;
   locations.forEach((location, i) => {
     location.distance = distance[i].distance.value;
   });
-  return locations;
+  return [null, locations];
 };
 
 module.exports = getLocations;
