184ecd436ba9bf3dce48a26d4e65f978cf28b33a
nfcarbone <nfcarbone@gmail.com>
2021-01-02 19:02:31 -0500

    add invalid zip error on getDistances

--- routes/common/reqs/getDistances.js
+++ routes/common/reqs/getDistances.js
@@ -7,6 +7,8 @@ const getDistances = async (req, res) => {
     const { zip, locationsZips, locations } = req.body;
     const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${zip}&destinations=${locationsZips}&key=${process.env.MAPS_API}`;
     const distanceData = await axios.get(url);
+    if (distanceData.data.origin_addresses[0] === '')
+      return res.status(400).send('Invalid zip code.');
     const distances = distanceData.data.rows[0].elements;
     locations.forEach((location, i) => {
       location.distance = distances[i].distance.value;
