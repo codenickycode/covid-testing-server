42e50cac3e7b17e41a9b6878ce6f9ee462abe611
nfcarbone <nfcarbone@gmail.com>
2021-02-15 16:43:28 -0500

    nodemailer sends temp password email

--- routes/common/reqs/forgotPassword.js
+++ routes/common/reqs/forgotPassword.js
@@ -1,17 +1,21 @@
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
 const { logger } = require('../../../logger');
 const User = require('../../../models/User.model.js');
+require('dotenv').config();
 
 const forgotPassword = async (req, res) => {
+  const email = req.body.email;
   try {
     let dbClient = await User.findOne({
-      email: { email: req.body.email },
+      email: { email },
     }).exec();
-    const random = Math.random().toString(36).substr(2, 8);
-    console.log(random);
-    const hash = bcrypt.hashSync(random, 12);
+    const password = Math.random().toString(36).substr(2, 8);
+    console.log(password);
+    const hash = bcrypt.hashSync(password, 12);
     dbClient.password = hash;
     await dbClient.save();
+    await sendPW(email, password);
     return res.status(200).send('Password reset');
   } catch (e) {
     logger.error(`/forgot => \n ${e.stack}`);
@@ -20,3 +24,21 @@ const forgotPassword = async (req, res) => {
 };
 
 module.exports = forgotPassword;
+
+const sendPW = async (email, password) => {
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
+  const info = await transporter.sendMail({
+    from: '"COVID Test Site" <codenickycode@gmail.com>',
+    to: email,
+    subject: 'Your temporary password',
+    text: `Your temporary password is: ${password}.  Please login and choose a new password.`,
+    html: `<h4>Your temporary password is:</h4><h1>${password}</h1><br><p>Please login and choose a new password.</p><p><a href='#'>COVID-19 Test Site</a>`,
+  });
+  logger.info('Message sent: %s', info.messageId);
+};
