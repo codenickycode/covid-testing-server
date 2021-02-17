7e326f72bf58ea0464bfbc32971279603aafc71c
nfcarbone <nfcarbone@gmail.com>
2021-02-17 13:07:42 -0500

    try more cors options

--- server.js
+++ server.js
@@ -28,7 +28,15 @@ mongoose
 // server settings
 const app = express();
 app.use(helmet({ contentSecurityPolicy: false }));
-app.use(cors({ credentials: true, origin: true, allowHeaders: '*' }));
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+    allowHeaders: '*',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  })
+);
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
