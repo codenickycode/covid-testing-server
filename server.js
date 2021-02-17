f63392a6ea26b15df76e41c7a3781e7385c336b0
nfcarbone <nfcarbone@gmail.com>
2021-02-17 15:23:37 -0500

    refresh deploy

--- server.js
+++ server.js
@@ -48,8 +48,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use((req, res, next) => res.status(401).send('sorry'));
-
 // morgan-body req and res logging
 morganBody(app, morganOptions);
 
